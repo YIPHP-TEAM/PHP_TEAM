8ba6b31d7953525879ef041ba3e73e5245cc458f
t-nagatomo <toshikibackn@gmail.com>
2020-12-28 15:46:13 +0900

    コントローラーとルーティングの作成
    コントローラの処理は書いていない

--- PHP_TEAM/routes/web.php
+++ PHP_TEAM/routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Front Controller
+Route::get('/login', 'Auth\LoginController@login');
+Route::get('/index', 'Front\IndexController@index');
+Route::get('/question', 'Front\IndexController@contact');
+Route::get('/user_point', 'Front\UserController@point');
+Route::get('/user_info', 'Front\UserController@info');
+Route::get('/item_detail', 'Front\ProductController@detail');
+Route::get('/cart', 'Front\CartController@index');
+Route::get('/cart_edit', 'Front\CartController@edit');
+Route::get('/cart_order', 'Front\OrderController@order');
