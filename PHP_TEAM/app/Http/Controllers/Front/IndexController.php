aac45c4dc1982e23e6a77572e066eed5343c072d
t-nagatomo <toshikibackn@gmail.com>
2021-01-15 16:00:10 +0900

    TOP画面リンク設定とかいろいろ

--- PHP_TEAM/app/Http/Controllers/Front/IndexController.php
+++ PHP_TEAM/app/Http/Controllers/Front/IndexController.php
@@ -14,10 +14,10 @@ class IndexController extends \App\Http\Controllers\Controller
      */
     public function index()
     {
-        // todo: products info get
-        // $this->data['products'] = ProductModel::all()->limit(8);
-        // dd($this->data['products']);
-
+        
+        // todo: 商品詳細テーブル(items)のデータを取得し、8件ユーザTOP画面に表示させるための処理。
+        // $this->data['items'] = ProductModel::all()->limit(8);
+        // dd($this->data['items']);
         // return view('front.index', $this->data);
         return view('front.index');
     }
@@ -40,23 +40,22 @@ class IndexController extends \App\Http\Controllers\Controller
     public function save(ContactRequest $request){
         // form solve
         //dd($request->all());
-
-
         // database save
+        //問い合わせテーブル(question)のインスタンス化。
         $question = new QuestionModel();
-        
         $question->user_id = $request->user_id;
         $question->content = $request->comment;
         $question->created_at = now();
         $question->save();
         //dd($question);
-
+        //問い合わせ画面完了ページに遷移するかもしれない処理。
         // complete redirect
         //return $this->complete();
         // return redirect('/question/complete');
         return redirect('/index');
     }
 
+    //完了した際のTOP画面にリダイレクトするための処理。
     public function complete()
     {
         return redirect('/index');
--- PHP_TEAM/resources/views/front/header.blade.php
+++ PHP_TEAM/resources/views/front/header.blade.php
@@ -1,8 +1,11 @@
 {{--共通フロントヘッダー--}}
 <a href="/index">ロゴ</a><br>
-<a href="#">管理者TOP画面<</a><br>
+@if (Auth::user()->role==1)
+<a href="#">管理者TOP画面<</a><br>    
+@endif
 <a href="/user_point">ポイント申請</a><br>
 <a href="/cart">カート内一覧</a><br>
 <a href="/user_info">ユーザ情報画面</a><br>
-<a href="/logout">ログアウト</a>
+<a href="/logout">ログア
+    ウト</a>
 
