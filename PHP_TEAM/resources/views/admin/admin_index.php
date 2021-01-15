c39c318009633661142b42fe3928a655d05c6436
西村　あかね <knknwkm@gmail.com>
2021-01-15 20:06:16 +0900

    1/15

---
+++ PHP_TEAM/resources/views/admin/admin_index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>管理者Top画面</title>
+<link rel="" href="">
+</head>
+<body>
+<header>
+<a href=""><img src="" alt="ブランドロゴ"></a>
+
+<a href="">お問合せ一覧画面</a>
+
+
+</header>
+
+<table>
+  
+    <tr>
+      <td>
+        
+      </td>
+      <td>
+      </td>
+      <td>
+      </td>
+    </tr>
+</table>
+</body>
+</html>
