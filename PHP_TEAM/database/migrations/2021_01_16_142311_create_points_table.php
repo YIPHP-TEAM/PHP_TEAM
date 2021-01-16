1d92eaf3eecbb1ec6aaee6fc3864c1d7214f7686
Nununta <yutayuki09040827@gmail.com>
2021-01-17 00:06:42 +0900

    マイグレーション 作成
    マイグレーション作成 、User.php、RegisterController.php編集

---
+++ PHP_TEAM/database/migrations/2021_01_16_142311_create_points_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->unsignedInteger('user_id'); //外部キー
+            $table->text('content');
+            $table->string('clear',15);
+            $table->timestamps();
+            $table->softDeletes();
+            
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('points');
+    }
+}
