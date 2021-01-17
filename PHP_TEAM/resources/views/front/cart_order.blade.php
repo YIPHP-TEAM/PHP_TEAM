4f699e116f75b626bd0a399f20a71e45669e3efb
t.sato <t-sato@y-i-group.co.jp>
2021-01-18 00:53:48 +0900

    update

--- PHP_TEAM/resources/views/front/cart_order.blade.php
+++ PHP_TEAM/resources/views/front/cart_order.blade.php
@@ -1,1 +1,50 @@
-購入画面
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="mx-auto" style="max-width:1200px">
+            <h1>購入画面</h1>
+            <!-- 購入商品一覧 -->
+            <div class="row">
+                <div class="">
+                    <div class=""><img src="" alt=""></div>
+                    <div class="">商品名</div>
+                    <div class="">必要ポイント数</div>
+                    <div class="">数量</div>
+                </div>
+                <br>
+                <div class="">
+                    <div class=""><img src="" alt=""></div>
+                    <div class="">商品名</div>
+                    <div class="">必要ポイント数</div>
+                    <div class="">数量</div>
+                </div>
+                <br>
+                <div class="">
+                    <div class=""><img src="" alt=""></div>
+                    <div class="">商品名</div>
+                    <div class="">必要ポイント数</div>
+                    <div class="">数量</div>
+                </div>
+            </div>
+
+            <!-- 購入確認 -->
+            <form action="#" method="post">
+                <div class="row">
+                    <div class="">
+                        <div class="">注文内容</div>
+                        <br>
+                        <div class="">
+                            <div class="">注文数合計</div>
+                            <br>
+                            <div class="">注文合計額</div>
+                        </div>
+                    </div>
+                    <div class="">
+                        <input type="submit" class="create-btn" value="購入する">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
--- PHP_TEAM/resources/views/layouts/app.blade.php
+++ PHP_TEAM/resources/views/layouts/app.blade.php
@@ -25,14 +25,11 @@
             <div class="container">
                 <!-- ロゴ -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('/img/freeks.png') }}" alt="">
+                <img src="{{ asset('/img/yi_point_logo.png') }}" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
-                <!-- ポイントの表示 -->
-                <div class="">ポイントの表示</div>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Right Side Of Navbar -->
@@ -40,6 +37,8 @@
                         <!-- Authentication Links -->
                         @guest
                         @else
+                            <!-- ポイントの表示 -->
+                            <div class="">ポイントの表示</div>
                             <li class="nav-item ml-2">
                                 <a class="nav-link text-black" href="#">ポイント申請</a>
                             </li>
@@ -70,14 +69,13 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
-
-        <footer>
+        <!-- フッター -->
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <a class="nav-link text-black" href="#">お問い合わせ</a>
-        </footer>
+        </nav>
     </div>
 </body>
 </html>
