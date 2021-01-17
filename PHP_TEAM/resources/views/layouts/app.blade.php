d94bf629ddb5d3648d28fb60d3e07754b21576f0
t.sato <t-sato@y-i-group.co.jp>
2021-01-18 01:29:32 +0900

    ヘッダー部分にregister追加

--- PHP_TEAM/resources/views/layouts/app.blade.php
+++ PHP_TEAM/resources/views/layouts/app.blade.php
@@ -35,7 +35,17 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
+                        <!-- ログインしてない時のヘッダー -->
                         @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        <!-- ログイン状態のヘッダー -->
                         @else
                             <!-- ポイントの表示 -->
                             <div class="">ポイントの表示</div>
