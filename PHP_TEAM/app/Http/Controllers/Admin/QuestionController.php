b4ac129276e166c070593dbb6702e9c7abbf5354
takeiroshiraishi <shirt8a1k0e@gmail.com>
2021-01-24 11:15:12 +0900

    コントローラの中身の記載

--- PHP_TEAM/app/Http/Controllers/Admin/QuestionController.php
+++ PHP_TEAM/app/Http/Controllers/Admin/QuestionController.php
@@ -7,5 +7,20 @@ use App\Http\Controllers\Controller;
 
 class QuestionController extends Controller
 {
-    //
+    //一覧表示
+    public function index() 
+    {
+        $questions = Question::all;
+        return view('admin.question');
+    }
+
+    //削除機能
+    public function delete(Request $request)
+    {
+        $question = Question::find($request->id);
+        $question->delete();
+        return redirect('/admin_index');
+    }
+
+
 }
--- PHP_TEAM/app/Http/Controllers/Admin/UserController.php
+++ PHP_TEAM/app/Http/Controllers/Admin/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return view('admin.index');
+        return view('admin_index');
     }
 
 
