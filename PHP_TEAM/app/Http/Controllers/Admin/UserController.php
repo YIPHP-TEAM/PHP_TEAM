ed8fca560c635b69cc132c16209eed4c258318c5
takeiroshiraishi <shirt8a1k0e@gmail.com>
2021-01-29 23:08:52 +0900

    user.phpの移動
    not find App\Userの解消

--- PHP_TEAM/app/Http/Controllers/Admin/UserController.php
+++ PHP_TEAM/app/Http/Controllers/Admin/UserController.php
@@ -10,23 +10,23 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        return view('admin_index');
+        // $users = User::all();
+        return view('admin.admin_index');
     }
 
     public function create(Request $request)
     {
-        $user = new UserModel();
-        $user->id = $request->id;
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->language = $request->language;
-        $user->role = $request->role;
-        //画像保存の追記
-        $question->created_at = now();
-        $question->save();
+        // $user = new UserModel();
+        // $user->id = $request->id;
+        // $user->name = $request->name;
+        // $user->email = $request->email;
+        // $user->language = $request->language;
+        // $user->role = $request->role;
+        // //画像保存の追記
+        // $question->created_at = now();
+        // $question->save();
 
-        return redirect('/admin_index'); 
+        // return redirect('/admin_index'); 
     }
 
 
