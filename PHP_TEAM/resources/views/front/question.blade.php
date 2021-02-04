a167be0166579630352103a9c2190b1a48f4f553
t.sato <t-sato@y-i-group.co.jp>
2021-02-05 08:24:42 +0900

    一般ユーザー画面レイアウト調整
    ・/public/img/yi_point_logo_l.pngの追加
    ・index.blade.phpの編集
    ・login.blade.phpの編集
    ・question.blade.phpの編集
    ・cart_complete.blade.phpの作成

--- PHP_TEAM/resources/views/front/question.blade.php
+++ PHP_TEAM/resources/views/front/question.blade.php
@@ -4,9 +4,9 @@
     <div class="container p-5">
       <div class="mx-auto">
         <h1 class="text-center border-bottom border-secondary pb-3 mb-5">お問い合わせ</h1>
+        <div class="form-group row px-5">
         <!-- ユーザーネーム -->
-        <p>{{ Auth::user()->name }} さん、こんにちは</p>
-        <div class="form-group row">
+        <p>こんにちは {{ Auth::user()->name }} さん。</p>
           <!-- 問い合わせフォーム -->
           @if ($errors->has('name'))
             <li>{{$errors->first('name')}}</li>
@@ -14,8 +14,10 @@
           <form action="/question/save" method="POST">
           @csrf
           <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-          <textarea name="comment" rows="4" cols="40"></textarea>
-          <input type="submit" value="お問い合わせ">
+          <textarea name="comment" rows="4" cols="120"></textarea>
+          <div class="col-md-12 clearfix">
+            <input class="float-right mt-2 p-2" type="submit" value="お問い合わせ">
+          </div>
           </form>
         </div>
       </div>  
