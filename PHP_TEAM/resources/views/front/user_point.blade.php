702e873707aaf1e2c4127b77835a88d4e7e90103
西村　あかね <knknwkm@gmail.com>
2021-02-10 01:35:10 +0900

    ポイント申請画面の調整
    ポイント申請画面
    cssの調整

--- PHP_TEAM/resources/views/front/user_point.blade.php
+++ PHP_TEAM/resources/views/front/user_point.blade.php
@@ -2,59 +2,32 @@
 
 @section('content')
 <div class="container p-5">
-        <div class="mx-auto">
-            <h1 class="text-center border-bottom border-secondary pb-3 mb-5">>ポイント申請画面</h1>
-                <form action="point/save" method="post">
-                    @csrf
-                    <div class="form-check">
-                        <input class="form-check-input" type="radio" name="clear" id="exampleRadios1" value="カリキュラムの合格">
-                        <label class="form-check-label" for="exampleRadios1">
-                        カリキュラムの合格 
-                        </label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input" type="radio" name="clear" id="exampleRadios1" value="その他の貢献活動">
-                        <label class="form-check-label" for="exampleRadios1">
-                        その他の貢献活動
-                        </label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input" type="radio" name="clear" id="exampleRadios1" value="資格取得">
-                        <label class="form-check-label" for="exampleRadios1">
-                        資格取得
-                        </label>
-                      </div>
-                      {!! formErr('clear') !!}
-                    {{-- <div class="form-group">
-                        <div class="">
-                            <input type="radio" class="form-control" name="title" value="{{  }}">
+        <h1 class="text-center border-bottom border-secondary pb-3 mb-5">ポイント申請画面</h1>
+          <form action="point/save" method="post">
+            @csrf
+                <div class="my-5 row">
+                    <h5 class="py-2 col-md-12">1.達成した項目を選択してください</h5>
+                    <div class="mx-auto my-2">
+                        <div class="btn-group btn-group-toggle" data-toggle="buttons" id="option1">
+                            <label class="btn btn-outline-secondary btn-lg">
+                            <input type="radio" name="options" id="option1" autocomplete="off">カリキュラムの合格
+                            </label>
+                            
+                            <label class="btn btn-outline-secondary btn-lg px-4">
+                            <input type="radio" name="options" id="option2" autocomplete="off">資格取得
+                            </label>
+
+                            <label class="btn btn-outline-secondary btn-lg">
+                            <input type="radio" name="options" id="option2" autocomplete="off">その他の貢献活動
+                            </label>
                         </div>
-                        <br>
-                        <label class="">カリキュラムの合格</label>
                     </div>
-                    <div class="form-group">
-                        <div class="">
-                            <input type="radio" class="form-control" name="title" value="{{  }}">
-                        </div>
-                        <br>
-                        <label class="">その他の貢献活動</label>
-                    </div>
-                    <div class="form-group">
-                        <div class="">
-                            <input type="radio" class="form-control" name="title" value="{{  }}">
-                        </div>
-                        <br>
-                        <label class="">資格取得</label>
-                    </div>
-                    <br> --}}
-                    <div class="form-group">
-                        <div class="">
-                            <textarea class="form-control" name="content" rows="10"></textarea>
-                        </div>
-                        {!! formErr('content') !!}
-                    </div>
-                    <input type="submit" class="create-btn" value="申請する">
-                </form>       
-        </div>
+                </div>
+                <div class="form-group">
+                    <h5 class="py-2">2.達成内容を入力してください</h5>
+                    <textarea class="form-control" name="content" rows="10"></textarea>
+                </div>
+                <input type="submit" class="create-btn btn-lg float-right" value="申請する">
+          </form>
 </div>
 @endsection
