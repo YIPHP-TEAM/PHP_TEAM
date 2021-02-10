85b888fc51232a627d85dedd47d9b58e4eb463a4
t.sato <t-sato@y-i-group.co.jp>
2021-02-10 23:18:42 +0900

    レイアウト調整
    ・admin.blade.php
    ->ユーザー一覧をテーブルに
    ・admin.question.blade.php
    ->お問い合わせ一覧をテーブルに
    ・user_info.blade.php
    ->テーブル調整

--- PHP_TEAM/resources/views/admin/admin_index.blade.php
+++ PHP_TEAM/resources/views/admin/admin_index.blade.php
@@ -4,29 +4,35 @@
     <div class="container px-5">
         <div class="mx-auto" >
             <!--↓↓ 検索フォーム ↓↓-->
-            <form class="form-group row mt-3 mb-5 py-2 pl-0" action="{{action('Admin\UserController@index')}}" method="get">
-                <div class="col-md-10">
-                    <input type="text" name="cond_title" value="{{$cond_title}}" class="form-control" placeholder="ワードを入力してください">
-                </div>
-                <div class="col-md-2">
+            <form class="row form-group mt-3 mb-5 py-2" action="{{action('Admin\UserController@index')}}" method="get">
+                <div class="col-md-8 d-flex mx-auto">
+                    <input type="text" name="cond_title" value="{{$cond_title}}" class="form-control mr-4" placeholder="ワードを入力してください">
                     <input type="submit" value="検索" class="btn btn-info">
                 </div>
             </form>
                 
             <!-- ユーザー一覧 -->
             <div class="row">
-                @foreach ($users as $user)
-                <div class="">
-                    <div class="">
-                    <!-- ユーザー画像 -->
-                        <a href="{{'/user_detail/'.$user->id}}"></a>
-                    </div>
-                    <div class=""><img src="{{asset('storage/img/'.$user->image)}}"><div class="">
-                        <div class="">{{$user->name}}</div>
-                        <div class="">{{$user->language}}</div>
-                    </div>
+                <div class="mx-auto col-md-8">
+                    <table class="table table-bordered text-center">
+                    <thead class="table-active">
+                        <tr>
+                        <th><div class="my-2">ユーザー画像</div></th>
+                        <th><div class="my-2">ユーザーネーム</div></th>
+                        <th><div class="my-2">選択言語</div></th>
+                        </tr>
+                    </thead>
+                    @foreach ($users as $user)
+                    <tbody class="table-light">
+                        <tr>
+                        <td><a href="{{'/user_detail/'.$user->id}}"><img src="{{asset('storage/img/'.$user->image)}}"></a></th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->language}}</td>
+                        </tr>
+                    </tbody>
+                    @endforeach
+                    </table>
                 </div>
-                @endforeach
             </div>
         </div>
     </div>
