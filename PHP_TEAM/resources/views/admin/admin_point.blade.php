9e75853113526d21e270e99b4ea545a74b4f7986
國松海斗 <k-kunimatsu@y-i-group.co.jp>
2021-02-18 03:37:44 +0900

    ユーザ詳細画面　追記

--- PHP_TEAM/resources/views/admin/admin_point.blade.php
+++ PHP_TEAM/resources/views/admin/admin_point.blade.php
@@ -1,42 +1,3 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>ポイント付与画面</title>
-    <link rel="" href="">
-  </head>
-
-  <body>
-    <div id="wrapper">
-        <!--▼グローバルナビゲーション部分-->
-        <?php include( $_SERVER['DOCUMENT_ROOT'] . '/admin_global_menu.php'); ?>
-
-        <article>
-            <h2>ポイント付与画面</h2>
-            <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
-            <h3><?php echo $row['ユーザ名']; ?></h3>
-            <section>
-                <h4>達成項目：<span><?php echo $row['達成項目']; ?></span></h4>
-                <h4>詳&nbsp;細：<span><?php echo $row['詳細']; ?></span></h4>
-                <p>？？削除ボタン？？</p>
-            </section>
-            <?php } ?>
-            <section>
-            <form action="">
-                <p>何ポイント付与しますか？</p>
-                <ul>
-                    <li>1P</li>
-                    <li>5P</li>
-                    <li>10P</li>
-                </ul>
-                <input type="submit" value="ポイント付与">
-            </form>
-            </section>
-        </article>
-    </div><!--wrapper-->
-  </body>
-</html>
-
 @extends('layouts.app')
 
 @section('content')
@@ -46,19 +7,31 @@
 
                 <div>
                   <!-- ユーザーネームの表示 -->
-                <div>{{ Auth::user()->name }}</div>  
+                <div>{{ $user->name }}</div>  
                 <div class="col-md-8">
-                        <div class="">達成項目名 {{$point->clear}}</div>
+                        <div class="">達成項目名 
+                              @foreach ($points as $point)
+                              {{$point->clear}}
+                              <br>
+                              @endforeach
+                        </div>
                         <br>
-                        <div class="">詳細内容{{$point->content}}</div>
+                        <div class="">詳細内容
+                              @foreach ($points as $point)
+                              {{$point->content}}
+                              <br>
+                              @endforeach
+                        </div>
                     </div>
                     <div class="col-md-4">
+                        <form action={{--"/admin_point/destroy/{{$points->id--}} method="post">
+
                         <button type="button" class="btn">削除</button>
                     </div>
                 </div>
                 <!-- ポイントの付与 -->
                 <div>
-                  <form action="point/save" method="post">
+                  <form class="form-inline py-3" name="quantity" action="{{'/user_detail/save/'.$user->id}}" method="post">
                       @csrf
                       <div class="form-check">
                           <input class="form-check-input" type="radio" name="clear" id="exampleRadios1" value="カリキュラムの合格">
@@ -78,9 +51,15 @@
                           10P
                           </label>
                         </div>
+                      <div class="col-md-2">
+                        <input type="hidden" name="{{$user->id}}" value="submit">
+                      <a href="{{'/user_detail/'.$user->id}}">
                       <input type="submit" class="create-btn" value="ポイント付与">
+                    </a>
+                      </div>
                   </form>   
                 </div>    
         </div>
 </div>
 @endsection
+ 
