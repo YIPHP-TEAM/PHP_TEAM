5474e14407bf23d0d10c0deb80103cf3a5015a0a
takeiroshiraishi <shirt8a1k0e@gmail.com>
2021-02-24 21:34:52 +0900

    Update Question.php
    倫理削除の実装

--- PHP_TEAM/app/Models/Question.php
+++ PHP_TEAM/app/Models/Question.php
@@ -3,10 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Question extends Model
 {
     public $timestamps = false;
+
+    use SoftDeletes;
 
     protected $table = 'questions';
 }
